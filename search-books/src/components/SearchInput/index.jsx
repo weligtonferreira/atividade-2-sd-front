0344417f8408b5d783194003026893aa2fb31423
DaveJosef <josedaaaaavid@gmail.com>
2022-03-25 21:41:29 -0300

    :construction: Modelo Simplificado de Busca de Livros

--- search-books/src/components/SearchInput/index.jsx
+++ search-books/src/components/SearchInput/index.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 import { Icon, Input, SearchDiv } from './style';
 
-export default function SearchInput() {
+export default function SearchInput(props) {
   const [searchContent, setSearchContent] = useState('');
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     console.log('Clicou');
+    props.onSubmit(searchContent);
+    e.preventDefault();
   };
 
   return (
-    <SearchDiv>
+    <SearchDiv onSubmit={handleSubmit}>
       <Input type='text' onChange={(e) => setSearchContent(e.target.value)} />
-      <Icon className='bi bi-search' onClick={() => handleSubmit} />
+      <button type='submit' style={{padding: '10px'}} className='btn btn-success align-center' onClick={() => handleSubmit}>
+        <Icon className='bi bi-search' />search something
+      </button>
     </SearchDiv>
   );
 }

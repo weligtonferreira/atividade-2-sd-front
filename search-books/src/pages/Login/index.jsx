ea5ab282ff672afb8636d11f99ba405d4a163c4d
José Ricardo <jricardobgoncalves@gmail.com>
2022-03-25 21:16:59 -0300

    Fazendo login com o Google

--- search-books/src/pages/Login/index.jsx
+++ search-books/src/pages/Login/index.jsx
@@ -1,28 +1,31 @@
-import React, { useContext, useEffect } from "react";
-import { LoginButton, LoginButtonImg, LoginButtonLabel } from "./styles";
-
-import googleIcon from "../../assets/search.png";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { GoogleLogin } from "react-google-login";
 import { AuthContext } from "../../contexts/Auth";
 
 export default function Login() {
   const { client, login, logout } = useContext(AuthContext);
 
-  // const initClient = () => {
-  //   const clientData = google.accounts.oauth2.initTokenClient;
-  // };
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   initClient();
-  // }, []);
+  const handleLogin = (response) => {
+    console.log(response);
+
+    login(response);
+    navigate("/");
+  };
 
   return (
     <main>
       <h1 className="App-title">Search Books</h1>
       <h3 className="App-text">Entre agora com a sua conta do Google</h3>
-      <LoginButton>
-        <LoginButtonImg src={googleIcon} />
-        <LoginButtonLabel>Continue with Google</LoginButtonLabel>
-      </LoginButton>
+      <GoogleLogin
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+        buttonText="Continue with Google"
+        onSuccess={handleLogin}
+        onFailure={handleLogin}
+        cookiePolicy={"single_host_origin"}
+      />
     </main>
   );
 }
